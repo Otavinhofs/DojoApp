c1ec33cebe5a5a25c61e269955f801b9304bb402
Otávio Fraga da Silva <otavinhofs1@gmail.com>
2022-12-16 15:33:44 -0300

    stack view, image view, view criadas

--- DojoAppViewCode/DojoAppViewCode/ViewController.swift
+++ DojoAppViewCode/DojoAppViewCode/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private let backgroundImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleToFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let bottomStackView: UIStackView = {
+        let stackview = UIStackView()
+        stackview.axis = .vertical
+        stackview.alignment = .center
+        stackview.spacing = 36
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        return stackview
+    }()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
