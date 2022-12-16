cd9109724baf4c672d020e8e671c3ab60ab92221
Otávio Fraga da Silva <otavinhofs1@gmail.com>
2022-12-16 15:20:31 -0300

    Add view code project

---
+++ DojoAppViewCode/DojoAppViewCode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DojoAppViewCode
+//
+//  Created by Otávio da Silva on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
